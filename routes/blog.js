0c9127096b32ef64186bfffe9c3021d91f789c2b
Akash Singh <furiousakash111@gmail.com>
2023-05-07 11:45:57 +0530

    Route for comment creation added

--- routes/blog.js
+++ routes/blog.js
@@ -1,2 +1,7 @@
 const express = require('express');
+const { createComment } = require('../controllers/commentController');
 const router = express.Router();
+
+router.post('/comments/create', createComment);
+
+module.exports = router;
