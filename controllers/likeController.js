83957a20b5d2caaa80bf97a5f497b93990cc6e9b
Akash Singh <furiousakash111@gmail.com>
2023-05-13 12:29:13 +0530

    Unlike post function also added

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -13,10 +13,32 @@ exports.likePost = async(req,res) => {
             .populate("likes").exec();
         res.status(200).json({
             post: updatedPost,
+            message: "Successfully liked the post !!",
         });
     } catch (error) {
         return res.status(500).json({
             message : "Couldn't like !!",
+            error: error,
+        })
+    }
+};
+
+//Unlike a post
+exports.unlikePost = async(req,res) => {
+    try {
+        const {post, like} = req.body;
+        //deleting from like schema
+        const deletedLike = await Like.findByIdAndDelete(like);
+        //updating the post schema
+        const updatedPost = await Post.findByIdAndUpdate(post, {$pull : {likes: deletedLike._id}}, {new: true})
+            populate("likes").exec();
+        res.status(200).json({
+            post: updatedPost,
+            message: "Unliked successfully !!",
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message : "Couldn't unlike !!",
             error: error,
         })
     }
