5e4f36036e735a5418ce40e4cb884f0d933db06d
Akash Singh <furiousakash111@gmail.com>
2023-05-13 12:05:08 +0530

    Router updated after adding like controller

--- routes/blog.js
+++ routes/blog.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { createComment } = require('../controllers/commentController');
 const { createPost } = require('../controllers/postController');
+const { likePost } = require('../controllers/likeController');
 const router = express.Router();
 
 router.post('/comments/create', createComment);
 router.post('/posts/create', createPost);
+router.post('/likes/like', likePost);
 module.exports = router;
